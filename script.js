8661feb2ddb40028d242034c7ada70f823ca855b
missesenam <moneyesenam@gmail.com>
2024-08-23 12:27:48 +0100

    auto part website homepage clone

---
+++ script.js
@@ -0,0 +1,6 @@
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 37.7749, lng: -122.4194 }, // Replace with   your desired center
+      zoom: 8
+  });
+}
